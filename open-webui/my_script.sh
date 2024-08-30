0715fe079dfda8c975e2565099de2fb3e21ec632
nguyenph <nguyenph.work@gmail.com>
2024-08-30 21:00:08 +0700

    init

---
+++ open-webui/my_script.sh
@@ -0,0 +1,4 @@
+docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -e OPENAI_API_KEY=token-abc123 -e OPENAI_API_BASE_URL=https://hoangpn.hungphu.org/v1 -e WEBUI_AUTH=True -v ./backend/data:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+
+
+docker compose -f docker-compose-ui-be.yaml up -d --build
